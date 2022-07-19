3d15770d6fef2a61e9b99e73bc972c23184dd244
Alistair70 <63516216+Alistair70@users.noreply.github.com>
2022-07-19 19:20:00 -0400

    Update inventoryConnection.java

--- pos/src/pos/inventoryConnection.java
+++ pos/src/pos/inventoryConnection.java
@@ -6,7 +6,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 
 public class inventoryConnection
